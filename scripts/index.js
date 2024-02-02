7a4ec06bd9f999e82c1e3c1883b12c34eb2dc6ee
Alexey Komin <alexeykomin@mail.ru>
2024-02-02 15:34:51 +0300

    Добавлен код для карточек V1.1

--- scripts/index.js
+++ scripts/index.js
@@ -1,33 +1,33 @@
 // @todo: Темплейт карточки
-const cardTmp = document.querySelector('#card-template').content;
+const cardTmp = document.querySelector("#card-template").content;
 
 // @todo: DOM узлы
-const placesList = document.querySelector('.places__list');
+const placesList = document.querySelector(".places__list");
 
 // @todo: Функция создания карточки
-function createCard(infoCard,deleteCard){
-    const cardElement = cardTmp.querySelector('.places__item').cloneNode(true);
-    const cardDEL = cardElement.querySelector('.card__delete-button');
+function createCard(infoCard, deleteCard) {
+  const cardElement = cardTmp.querySelector(".places__item").cloneNode(true);
+  const cardDEL = cardElement.querySelector(".card__delete-button");
 
-    cardElement.querySelector('.card__image').src = infoCard.link;
-    cardElement.querySelector('.card__image').alt = infoCard.name;
-    cardElement.querySelector('.card__title').textContent = infoCard.name;
+  cardElement.querySelector(".card__image").src = infoCard.link;
+  cardElement.querySelector(".card__image").alt = infoCard.name;
+  cardElement.querySelector(".card__title").textContent = infoCard.name;
 
-    cardDEL.addEventListener('click', () => {
-        deleteCard(cardElement);
-        console.info(`Удалена карточка: ${infoCard.name}`);
-    });
-    
-    return cardElement;
+  cardDEL.addEventListener("click", () => {
+    deleteCard(cardElement);
+    console.info(`Удалена карточка: ${infoCard.name}`);
+  });
+
+  return cardElement;
 }
 
 // @todo: Функция удаления карточки
 function deleteCard(cardElement) {
-    cardElement.remove();
+  cardElement.remove();
 }
 
 // @todo: Вывести карточки на страницу
-initialCards.forEach(infoCard => {
-    placesList.append(createCard(infoCard, deleteCard));
-    console.info(`Добавлена карточка: ${infoCard.name}`);
+initialCards.forEach((infoCard) => {
+  placesList.append(createCard(infoCard, deleteCard));
+  console.info(`Добавлена карточка: ${infoCard.name}`);
 });
